425b3317f0a238c7a07f43915347f1bef55e5f23
buruTF <104819036+burutf@users.noreply.github.com>
2024-06-03 01:35:19 +0800

    改成了分片上传，后面可以做中断处理

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,44 @@
-import {configOss} from '@/utils/oss/configOss'
+import { configOss } from '@/utils/oss/configOss'
 import http from '@/utils/axios'
 
 
 //获取sts临时令牌
-export const stsOSS = ()=>{
+export const stsOSS = () => {
     return http.get('/sts')
 }
 
-//上传文件
-export const upload = async (FileName,FileUrl)=>{
-    //处理字符串，只保留url
-    // const url = FileUrl.slice(FileUrl.indexOf(':')+1)
+
+// //分片上传时获取id
+// export const inituploadid = async (FileName)=>{
+//     const clientOSS = await configOss()
+//     const res = await clientOSS.initMultipartUpload(FileName)
+//     return res
+// }
+// //开始分片，进行上传
+// export const partupload = async (name, uploadId, partNo, file, start, end)=>{
+//     const clientOSS = await configOss()
+//     const partResult = await clientOSS.uploadPart(name, uploadId, partNo, file, start, end);
+//     return partResult
+// }
+
+//上传文件（分片上传）
+export const upload = async (FileName, fileobj, options) => {
     const clientOSS = await configOss()
-    // //开始上传
-    const putdata = await clientOSS.put(FileName,FileUrl)
+    //开始上传
+    const putdata = await clientOSS.multipartUpload(FileName, fileobj, { ...options })
+    clientOSS.cancel();
+
     return putdata
+}
+//上传过程中，删除已经上传了但是不需要的文件
+export const delupload = async (FileName) => {
+    const clientOSS = await configOss()
+    const delfile = await clientOSS.delete(FileName)
+    return delfile
+}
+
+//取消上传
+export const cancelupload = async ()=>{
+    const clientOSS = await configOss()
+    clientOSS.cancel();
 }
--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -4,36 +4,33 @@
       class="upload-demo"
       action=""
       ref="upload"
-      accept="#"
       multiple
-      :limit="120"
+      :limit="50"
       :file-list="fileList"
       :http-request="handleUploadFile"
       :before-upload="beforeUpload"
       :before-remove="beforeremove"
       :on-change="onchange"
-      list-type="picture"
     >
-      <el-button size="small" type="primary">选取文件</el-button>
-      <el-button
-        style="margin-left: 10px"
-        size="small"
-        type="success"
-        @click="submitUpload"
-        >上传到服务器</el-button
-      >
+      <el-button size="small" type="primary">上传文件</el-button>
     </el-upload>
     {{ fileList }}
+    {{ filefpid }}
   </div>
 </template>
 
 <script>
+
 export default {
   name: "UploadFile",
   data() {
     return {
       dada: {},
       fileList: [],
+      userid:10001,
+      uploadTemUrl:"temporary/",
+      //每个分片上传文件的id
+      filefpid:[]
     };
   },
   methods: {
@@ -43,32 +40,114 @@ export default {
       this.$nextTick(async () => {
         const {name} = filec.file
         const {file} = filec
-        const uploadreturn = await this.$API.upload(name,file);
-        console.log(uploadreturn);
-
-        //表单数据收集还没做，要考虑文件上传什么时候传
-        //今天该完善上传完后的数据整理上传到服务器、用户反馈
+        const uploadreturn = await this.$API.upload(`${this.temlurl}${name}`,file,this.fhopiton());
+        
 
 
+        // //获取每个文件分片上传时的ID
+        // const resid = await this.$API.inituploadid(`${this.temlurl}${name}`)
+        // //名字
+        // const namefp = resid.name
+        // //id
+        // const uploadId = resid.uploadId
+        // this.filefpid.push({
+        //   [namefp]:uploadId
+        // })
+        // console.log(uploadId);
+        // //开始分片进行上传，中途可停止
+        // const partupload = await this.$API.partupload(1,uploadId)
+        // console.log(partupload);
+
+
+
+
+        //执行这个函数可以给文件列表后面打钩
+        filec.onSuccess()
       });
     },
     //上传之前做校验
     beforeUpload(file) {
-      
+      //拿到文件的类型和大小
+      const {type,size} = file;
+      const istype = type === "video/mp4"
+      const issize = size/1024/1024 < 1024*4   //乘号后面单位GB，当前是最大不超过4GB
+      if (!istype) {
+        this.$message.error('目前只支持MP4格式，请重新上传');
+      }
+      if (!issize) {
+        this.$message.error('最大文件不能超过4GB，请重新上传');
+      }
+      return istype&&issize
     },
     //删除文件时也要更新列表
-    beforeremove(file, fileList) {
-      this.fileList = fileList;
+    async beforeremove(file, fileList) {
+      
+      const {name} = file
+      //删除OSS里的文件
+      const delfileres = await this.$API.delupload(`${this.temlurl}${name}`)
+      if (delfileres.res.status===204) {
+        this.$nextTick(() => {
+          this.fileList = fileList
+        })
+      }
     },
     //文件列表发生更改时，更新fileList数组
     onchange(file, fileList) {
-      this.fileList = fileList;
+      this.$nextTick(() => {
+        this.fileList = fileList
+      })
     },
     submitUpload() {
       this.$refs.upload.submit();
     },
+    //分片上时的配置项
+    fhopiton(){
+      const options = {
+        // 获取分片上传进度、断点和返回值。
+        progress: (p, cpt, res) => {
+          //最后一个分片不执行
+          if (p!==1){
+            //下面这一段是，获取名字和分片初始化时的id，再添加到一个数据里面，方便后面中断操作，数组有做去重处理
+            const {name,uploadId} = cpt
+            const isexist = Boolean(this.filefpid.find((item,i)=>{
+              return item.name === name
+            }))
+            if (!isexist) {
+              this.filefpid.push({name:name,uploadId:uploadId})
+            }
+          }
+        },
+        // 设置并发上传的分片数量。
+        // parallel: 4,
+        // 设置分片大小。默认值为1 MB，最小值为100 KB。
+        // partSize: 1024 * 1024,
+      }
+      return options
+    }
   },
+  computed:{
+    //拼接
+    temlurl(){
+      return `${this.userid}/${this.uploadTemUrl}`
+    }
+  }
 };
 </script>
 
-<style></style>
+<style lang="less">
+.upload-demo {
+    display: flex;
+    align-items: center;
+    height: 150px;
+    border: 1px solid black;
+    margin: 10px;
+    .el-upload{
+      flex: 2;
+    }
+    .el-upload-list{
+      overflow: auto;
+      height: 100%;
+      flex: 8;
+    }
+}
+</style>
