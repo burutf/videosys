d01f9197f5b271b5e25266ef5e15a40d5b9190d0
buruTF <104819036+burutf@users.noreply.github.com>
2024-08-04 15:01:18 +0800

    打包优化，启用gzip，解决包太大的问题

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,30 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+//gzip预压缩
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
   ...defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
   }),
   devServer: {
-    proxy:{
+    proxy: {
       //当请求前缀是这个时就代理请求
-      '/api':{
+      "/api": {
         //请求的地址
-        target:'http://yfengxing.cn:3030',
-      }
-    }
+        target: "http://yfengxing.cn:3030",
+      },
+    },
   },
-}
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        filename: "[path][base].gz", // 压缩后的文件名
+        algorithm: "gzip", // 压缩算法
+        test: /\.(js|css|html|svg)$/, // 匹配的文件类型
+        threshold: 10240, // 只有大小大于该值的文件会被压缩，单位为字节
+        minRatio: 0.8, // 压缩比率小于这个值的文件才会被压缩
+        deleteOriginalAssets: false,//是否删除源文件
+      }),
+    ],
+  },
+};
